f5333a28bd5d50f3a3c317cf734838ef5af93b4e
Alptekin <67191456+AlptekinOcakdan@users.noreply.github.com>
2022-09-08 15:30:05 +0300

    Building a MERN Project

--- server/index.js
+++ server/index.js
@@ -5,14 +5,16 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 
 import postRoutes from './routes/posts.js';
+import userRouter from "./routes/user.js";
 
 const app = express();
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(express.json({ limit: '30mb', extended: true }))
+app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
+app.use("/user", userRouter);
 
 const CONNECTION_URL = 'mongodb+srv://alptekin:Alptekin-52@cluster0.mwi2v9g.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
