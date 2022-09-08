853829d96df1be6b4fceebec931d53a4bd57dbb6
Alptekin <67191456+AlptekinOcakdan@users.noreply.github.com>
2022-09-08 13:45:57 +0300

    Building a MERN Project

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
@@ -14,9 +15,10 @@ app.use(cors());
 app.use('/posts', postRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://alptekin:Alptekin-52@cluster0.mwi2v9g.mongodb.net/?retryWrites=true&w=majority';
-const PORT = process.env.PORT|| 5001;
+const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () => console.log(`Server Running   on Port: http://localhost:${PORT}`)))
-    .catch((error) => console.log(`${error} did not connect`));
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
 
+mongoose.set('useFindAndModify', false);
